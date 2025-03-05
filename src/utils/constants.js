5292be42a09329ce6b650d80f0973a3c4e0a9c69
Amishakul <awadhutkavita@gmail.com>
2025-03-05 17:40:38 +0530

    made changes in url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const GOOGLE_API_KEY = "AIzaSyAW3Bh7oAW8krEB_QHZjRw7Rv4lQ18Yncw";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=56&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVE_CHAT = 25;
 
